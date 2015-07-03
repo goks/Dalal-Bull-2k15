f922c38b6831b0996be2282742f048fa3c8c6277
harikishen <hari751995@gmail.com>
2015-07-03 20:15:35 +0530

    3-July-2k15-Hari

--- Dalal-Bull-2k15-django/dalalbull/login/urls.py
+++ Dalal-Bull-2k15-django/dalalbull/login/urls.py
@@ -3,10 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index')
-	#url(r'^invalid/$', views.invalid_login, name='invalid_login'),
-	#url(r'^login/$', views.login, name='login'),
-	#url(r'^dashboard/$', views.dashboard, name='dashboard'),
-	#url(r'^auth/$', views.auth_view, name='auth_view'),
-	#url(r'^logout/$', views.logout, name='logout'),
+	url(r'^$', views.index, name='index'),
+	url(r'^login$', views.login, name='login'),
+	url(r'^dashboard$', views.dashboard, name='dashboard'),
 ]
